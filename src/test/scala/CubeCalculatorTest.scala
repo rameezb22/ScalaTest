ad8c04e52279f5c3bb86354c0408d9a2df5ef544
rameezb22 <rameez.b.bhat@gmail.com>
2022-04-11 17:40:18 +0530

    three test

--- src/test/scala/CubeCalculatorTest.scala
+++ src/test/scala/CubeCalculatorTest.scala
@@ -1,9 +1,15 @@
-class CubeCalculatorTest extends AnyFunSuite {
+import org.scalatest.funsuite.AnyFunSuite
+    
+   class CubeCalculatorTest extends AnyFunSuite {
        test("CubeCalculator.cube 3 should be 27") {
            assert(CubeCalculator.cube(3) === 27)
        }
 
        test("CubeCalculator.cube 4 should be 63") {
            assert(CubeCalculator.cube(4) === 63)
+       }
+
+       test("CubeCalculator.cube 0 should be 0") {
+           assert(CubeCalculator.cube(0) === 0)
        }
    }
